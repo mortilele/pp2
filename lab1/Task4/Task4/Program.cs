bfe00010ffdcd3da694ecfa1e25784aef00f176a
MarTiaN <ali.mars.99@gmail.com>
2019-01-26 11:39:03 +0600

    task4

---
+++ lab1/Task4/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n-i; j <= n; j++)
+                {
+                    Console.Write("[*]");
+                }
+                Console.WriteLine();
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
