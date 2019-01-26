3b642401b4f1b304f5099fd38a374581ec519a58
MarTiaN <ali.mars.99@gmail.com>
2019-01-26 11:25:26 +0600

    task2

---
+++ lab1/Task2/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        class Student
+        {
+            string name;
+            string id;
+            int year = 0;
+
+            public Student(string name, string id)
+            {
+                this.name = name;
+                this.id = id;
+            }
+
+            public void print()
+            {
+                Console.WriteLine("Name of student: {0}", name);
+                Console.WriteLine("Id of student: " + id);
+            }
+
+            public void incr()
+            {
+                year++;
+                Console.WriteLine("Year of student increased by one, now is {0}", year);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Alik", "18BD12312");
+            s1.print();
+            s1.incr();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab1/Task3/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            string[] ss = Console.ReadLine().Split();
+
+        }
+    }
+}
