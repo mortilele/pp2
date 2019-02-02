d95bb23675cb22c5cd6a856b352233d6e6da2b4d
mortilele <ali.mars.99@gmail.com>
2019-02-02 10:45:06 +0600

    tasks added

---
+++ template.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace prog
+{
+    class prog
+    {
+            /*
+	public static void ex()
+	{
+		string p = @"C:\Users\User\Desktop\Codi\inputs.txt";
+		FileInfo file = new FileInfo(p);
+		Console.WriteLine(file.name);
+	}    */
+	public static void Main()
+    {
+		            
+    }
+}
+}
