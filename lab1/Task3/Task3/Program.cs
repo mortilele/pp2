ff88f2355069a48249812ff6fa92afdb8aa93617
mortilele <ali.mars.99@gmail.com>
2019-02-16 10:39:03 +0600

    updated lab1 task3

--- lab1/Task3/Task3/Program.cs
+++ lab1/Task3/Task3/Program.cs
@@ -11,15 +11,16 @@ namespace Task3
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine()); //read size of array
-            int[] a = new int[n]; //create array of size n
+            List<int> a = new List<int>();//create array of size n
             string[] ss = Console.ReadLine().Split(); //split sequence
             for (int i = 0; i < n; i++)
             {
-                a[i] = int.Parse(ss[i]); //from string to int each element 
+                a.Add(int.Parse(ss[i])); //from string to int each element 
+                a.Add(int.Parse(ss[i])); //two times
             }
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < a.Count; i++)
             {
-                Console.Write("{0} {0} ", a[i]); //double output each element of array
+                Console.Write(a[i] + " "); //double output each element of array
             }
             Console.ReadLine();
         }
