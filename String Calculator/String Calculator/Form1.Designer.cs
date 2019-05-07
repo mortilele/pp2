3b4c904380dea7f53b6cad32685459c87d18d86f
mortilele <ali.mars.99@gmail.com>
2019-05-07 16:10:34 +0600

    bonus 2

---
+++ String Calculator/String Calculator/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace String_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(380, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(145, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(182, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ans";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 213);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "String Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ String Calculator/String Calculator/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace String_Calculator
+{
+    public partial class Form1 : Form
+    {
+        List<string> operators;
+        string input;
+        public Form1()
+        {
+            InitializeComponent();
+            operators = new List<string>(6) { "(", ")", "+", "-", "*", "/" };
+        }
+
+        public int Priority(string s)
+        {
+            if (s == "(" || s == ")")
+                return 0;
+            if (s == "+" || s == "-")
+                return 1;
+            if (s == "*" || s == "/")
+                return 2;
+            return 3;
+        }
+
+        public List <string> Divide_and_Conquer()
+        {
+            List<string> output = new List<string>();
+            int i = 0;
+            while (i < input.Length)
+            {
+                string k = input[i] + "";
+                if (char.IsDigit(input[i]))
+                {
+                    int j = i + 1;
+                    while (j < input.Length && char.IsDigit(input[j]))
+                    {
+                        k += input[j];
+                        j++;
+                    }
+                }
+                output.Add(k);
+                i += k.Length;
+            }
+            return output;
+        }
+
+        public List <string> ConvertOPN(List <string> divided)
+        {
+            List<string> operations = new List<string>();
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < divided.Count; i++)
+            {
+                if (operators.Contains(divided[i]))
+                {
+                    if (stack.Count > 0 && divided[i] != "(")
+                    {
+                        if (divided[i] == ")")
+                        {
+                            string k = stack.Pop();
+                            while (k != "(") //add all operations inside branches
+                            {
+                                operations.Add(k);
+                                k = stack.Pop();
+                            }
+                        }
+                        else if (Priority(divided[i]) > Priority(stack.Peek())) //less priority in stack just add to stack
+                        {
+                            stack.Push(divided[i]);
+                        }
+                        else
+                        {
+                            while (stack.Count > 0 && Priority(divided[i]) <= Priority(stack.Peek())) // (+ -) || (* /)
+                            {
+                                operations.Add(stack.Pop());
+                            }
+                            stack.Push(divided[i]);
+                        }
+                    }
+                    else
+                    {
+                        stack.Push(divided[i]); // if stack empty
+                    }
+                }
+                else
+                {
+                    operations.Add(divided[i]); //if number
+                }
+            }
+            //remaining
+            if (stack.Count > 0)
+            {
+                while (stack.Count > 0)
+                {
+                    string temp = stack.Pop();
+                    operations.Add(temp);
+                }
+            }
+            return operations;
+        }
+
+        public int Operate(string oper, int a, int b)
+        {
+            if (oper == "+")
+                return a + b;
+            if (oper == "-")
+                return b - a;
+            if (oper == "/")
+                return b / a;
+            return b * a;
+        }
+
+
+        public int Calculate(List <string> opn)
+        {
+            Stack<string> stack = new Stack<string>();
+            int i = 0;
+            string k = opn[i];
+            int size = opn.Count;
+            size--;
+            while (size >= 0)
+            {
+                if (!operators.Contains(k)) //add numbers to stack
+                {
+                    stack.Push(k);
+                    i++;
+                    k = opn[i];
+                    size--;
+                }
+                else
+                {
+                    int a = Convert.ToInt32(stack.Pop()); //meet two last numbers and last operations
+                    int b = Convert.ToInt32(stack.Pop());
+                    int result = Operate(k, a, b);
+                    stack.Push(Convert.ToString(result));
+                    if (size > 0)
+                    {
+                        i++;
+                        k = opn[i];
+                        size--;
+                    }
+                    else
+                        break; 
+                }
+            }
+            return Convert.ToInt32(stack.Pop());
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            input = textBox1.Text;
+            List<string> divided = Divide_and_Conquer(); //process input string
+            List<string> opn = ConvertOPN(divided); // convert to notation
+            int ans = Calculate(opn); //calculate
+            string d = ""; 
+            for (int i = 0; i < opn.Count; i++)
+            {
+                d += opn[i] + ",";
+            }
+            label1.Text = ans + "";
+            textBox1.Text = d;
+        }
+    }
+}
