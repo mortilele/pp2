03e4a6fa4d9c189309a6660d36745b854541c636
mortilele <ali.mars.99@gmail.com>
2019-05-13 16:51:53 +0600

    quiz4

---
+++ quiz4/quiz4/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quiz4
+{
+    public partial class Form1 : Form
+    {
+        int res = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            int temp = Convert.ToInt32(b.Text);
+            temp++;
+            if (temp % 2 == 0)
+                res++;
+            b.Text = temp + "";
+            textBox1.Text = res + "";
+        }
+    }
+}
